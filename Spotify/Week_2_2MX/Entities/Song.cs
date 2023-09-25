f7610389659636639336c55b2c457ad757c52a4d
Celal Karahan <53397470+k-celal@users.noreply.github.com>
2023-09-25 19:25:12 +0300

    New Project

---
+++ Spotify/Week_2_2MX/Entities/Song.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_2MX.Entities
+{
+    internal class Song
+    {
+        public Guid Id { get; set; }   
+        public string Title { get; set; }
+        public string Composer { get; set; }
+        public string Artist { get; set; }
+        public string Producer { get; set; }
+        public Song() 
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Song(string title, string composer, string artist, string producer) :this()
+        {
+            Title = title;
+            Composer = composer;
+            Artist = artist;
+            Producer = producer;
+            
+        }
+    }
+}
