bb61487ed517121f3d7ea39afb6e2fb378ec85cb
k-celal <celal51kara@gmail.com>
2023-11-20 02:09:06 +0300

    [Feat]Week10 Homework1
    Static variable

---
+++ MoodleOdev/Hafta10Odev/Hafta10Odev/ConfigurationService.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+public class ConfigurationService
+{
+    private static ConfigurationService instance;
+    public static ConfigurationService GetInstance()
+    {
+        if (instance is null)
+            instance= new ConfigurationService();
+        return instance;
+    }
+    public string GetValue(string key)
+    {
+        ConfigurationManager configurationManager = new();
+        string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        configurationManager.SetBasePath(path);
+        configurationManager.AddJsonFile("Configuration.json");
+        return configurationManager.GetSection(key).Value;
+
+    }
+    private ConfigurationService()
+    {
+        Console.WriteLine("Instance Created");
+    }
+}
---
+++ MoodleOdev/Hafta10Odev/Hafta10Odev/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region
+//MyClass mycs = new MyClass();
+//MyClass mycs2 = new MyClass();
+//MyClass mycs3 = new MyClass();
+//MyClass mycs4 = new MyClass();
+
+//class MyClass
+//{
+//    //SADECE ilk  nesne için üretilir
+//    static MyClass()
+//    {
+//        Console.WriteLine("Static Constructor");
+//    }
+
+//    public MyClass()
+//    {
+//        Console.WriteLine("Instance create");
+//    }
+//}
+#endregion
+
+Console.WriteLine("Singleton");
+
+ConfigurationService configurationService = ConfigurationService.GetInstance();
+ConfigurationService.GetInstance();
+ConfigurationService.GetInstance();
+ConfigurationService.GetInstance().GetValue("ConnectionStrings:ExampleDb"); ;
+//var azureConnString = configurationService.GetValue("ConnectionStrings:ExampleDb");
+
